582cc5fcbc4d9533af0814bc5f3db1dc4aaf600f
Sirin Koca <s182211@oslomet.no>
2022-05-20 00:00:15 +0200

    update bilkunde.js

--- src/main/resources/static/bilkunde.js
+++ src/main/resources/static/bilkunde.js
@@ -1,12 +1,45 @@
-$(function(){
-    hentAlleKunder();
-});
+function lagreKunde() {
+    const bilKunde = {
+        fornavn: $("#fornavn").val(),
+        etternavn: $("#etternavn").val(),
+        eierBil: $("input[name=valgtBil]:checked").val(),
+        forerkort: $("input[name=forerkort]:checked").val()
+    };
+    const url = "/lagreKunde";
 
-function hentAlleKunder(){
-    $.get("/hentkunder", function(kunder){
+    $.post(url, bilKunde, function () {
+        hentAlleKunder();
+    });
+
+    $("#fornavn").val(""),
+    $("#etternavn").val(""),
+    $("input[name=valgtBil]:checked").val(""),
+    $("input[name=forerkort]:checked").val("")
+}
+
+function hentAlleKunder() {
+    $.get("/hentBiler", function (kunder) {
         formaterKunder(kunder);
     });
 }
-function formaterKunder(){
-    let ut = "<table><tr><th>Fornavn</th><th>Etternavn</th><th>eierBil</th></tr>"
+
+function formaterKunder(kunder) {
+    let ut = "<table><tr><th>Fornavn</th><th>Etternavn</th><th>eierBil</th><th>Førerkort</th></tr>";
+    for (let kunde of kunder) {
+        ut += "<tr><td>" + kunde.fornavn +
+            "</td><td>" + kunde.etternavn +
+            "</td><td>" + kunde.eierBil +
+            "</td><td>" + kunde.forerkort +
+            "</td></tr>";
+    }
+    ut += "<table>";
+    $("#bilKundene").html(ut);
 }
+
+function slettBilKunder() {
+    $.get("/slettAlleKunder", function () {
+        hentAlleKunder();
+        $("#bilKundene").html(null);
+    });
+}
+
